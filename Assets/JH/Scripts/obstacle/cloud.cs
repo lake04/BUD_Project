3f6bf82339f4758d219e0510216779e2adf58319
lake04 <wjdghksrla0409@gmail.com>
2025-06-05 00:28:53 +0900

    구름 추가

--- Assets/JH/Scripts/obstacle/cloud.cs
+++ Assets/JH/Scripts/obstacle/cloud.cs
@@ -4,13 +4,24 @@ using UnityEngine;
 
 public class cloud : MonoBehaviour
 {
+    [SerializeField] private GameObject effect;
+
+
     void Start()
     {
-        
+        effect.SetActive(true);
     }
 
     void Update()
     {
         
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            //이거는 태민이가 플레이어 작업 하면 추가함
+        }
     }
 }
